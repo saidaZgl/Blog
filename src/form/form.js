8d44aa1622c3c08024895409f72abd2023d8b21b
Saida Zghal <zghal.saida@orange.fr>
2020-04-21 23:33:32 +0200

    mise en place du JS pour le formulaire

--- src/form/form.js
+++ src/form/form.js
@@ -1,2 +1,33 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
+
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    const json = JSON.stringify(article);
+    //fetch
+  }
+});
+
+const formIsValid = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Vous devez renseigner tous les champs");
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+  } else {
+    errorElement.innerHTML = "";
+  }
+};
