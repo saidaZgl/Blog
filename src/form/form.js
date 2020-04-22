b86a403c15f067c489546e76b2afd34eb366858b
Saida Zghal <zghal.saida@orange.fr>
2020-04-22 12:29:07 +0200

    création d'un article et envoi au serveur

--- src/form/form.js
+++ src/form/form.js
@@ -5,13 +5,25 @@ const form = document.querySelector("form");
 const errorElement = document.querySelector("#errors");
 let errors = [];
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const article = Object.fromEntries(formData.entries());
   if (formIsValid(article)) {
-    const json = JSON.stringify(article);
-    //fetch
+    try {
+      const json = JSON.stringify(article);
+      const response = await fetch("https://restapi.fr/api/article", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const body = await response.json();
+      console.log(body);
+    } catch (e) {
+      console.log("e : ", e);
+    }
   }
 });
 
@@ -27,7 +39,9 @@ const formIsValid = (article) => {
       errorHTML += `<li>${e}</li>`;
     });
     errorElement.innerHTML = errorHTML;
+    return false;
   } else {
     errorElement.innerHTML = "";
+    return true;
   }
 };
